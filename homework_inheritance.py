d961a88ceed8ec90deca28f422d76e6b63c2666d
Vladislav <v.janibekyan@gmail.com>
2021-01-10 22:31:05 +0200

    final version

--- homework_inheritance.py
+++ homework_inheritance.py
@@ -33,43 +33,49 @@ fifth_stage.presentation()
 
 # 2.
 class Country:
-	def __init__(self,name,continent):
-		self.name = name
-		self.continent = continent
-	def Return_name(self):
-		return name,continent
+    def __init__(self,name,continent):
+        self.name = name
+        self.continent = continent
+    def Return_name(self):
+        return name,continent
 class Brand:
-	def __init__(self,brand_name, start_date):
-		self.brand_name = brand_name
-		self.start_date = start_date
-	def presentation(self):
-		return f"{brand_name} was created {start_date}. "
+    def __init__(self,brand_name, start_date):
+        self.brand_name = brand_name
+        self.start_date = start_date
+    def presentation(self):
+        return f"{self.brand_name} was created {self.start_date}. "
 class Season:
-	def __init_(self,season_name, average_temperature):
-		self.season_name = season_name
-		self.average_temperature = average_temperature
-	def presentation_season(self):
-		return f"in {season_name} the average temperature is {average_temperature}"
+    def __init__(self,season_name, average_temperature):
+        self.season_name = season_name
+        self.average_temperature = average_temperature
+    def presentation_season(self):
+        return f"in {self.season_name} the average temperature is {self.average_temperature}"
 class Product(Country, Brand, Season):
-	def __init(self, product_name, product_type, product_price, product_quantity):
-		self.product_name = product_name
-		self.product_type = product_type
-		self.product_price = product_price
-		self.product_quantity = product_quantity
-		Country.__init__(self, name, continent)
-		Brand.__init__(self, brand_name, start_date)
-		Season.__init__(self, season_name, average_temperature)
-	def present_product(self):
-		return print(f"this is {product_name} it is {product_type} the price is {product_price} and we have {product_quantity} left.")
+    def __init__(self, name, continent, product_name, product_type, product_price, product_quantity,brand_name, start_date, season_name, average_temperature):
+        Country.__init__(self, name, continent)
+        Brand.__init__(self,brand_name, start_date)
+        Season.__init__(self,season_name, average_temperature)
+        self.product_name = product_name
+        self.product_type = product_type
+        self.product_price = product_price
+        self.product_quantity = product_quantity
+    def present_product(self):
+        return f"this is {self.product_name} it is {self.product_type} the price is {self.product_price} and we have {self.product_quantity} left."\
+                F"the brand is {self.presentation()} the season is {self.presentation_season()}"
+    def discount(self, discount_persenatge):
+        self.product_price = self.product_price / 100 * (100- discount_persenatge)
+        return self.product_price
+    def quantity_increase(self):
+    	self.product_quantity = self.product_quantity + 12 
 
-	def discount(self, discount_persenatge):
-		product_price = product_price / 100 * (100- discount_persenatge)
-		return product_price
-	def quantity_increase(self):
-		product_quantity = product_quantity + 10 
-		return product_quantity
 
-check = Product("belt", "accesories", 500, 24, "USA", "america", "Gucci", 1965, "winter", 3)
+check = Product("name_for_country", "value_for_continent","wine","drink", 2000, 12, "Armas","2010","Aoutumn","30",)
+
+print("before modification ", check.present_product())
+check.discount(30)
+check.quantity_increase()
+
+print("after modification ", check.present_product())
 
 
 
