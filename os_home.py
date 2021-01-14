1d6539da880a4dac96cfee338550aef279da6b84
VladislavJanibekyan <76608648+VladislavJanibekyan@users.noreply.github.com>
2021-01-14 17:19:45 +0200

    Update os_home.py

--- os_home.py
+++ os_home.py
@@ -1,8 +1,8 @@
 import os
 print("begining:", os.getcwd())
+path = os.getcwd()
 os.mkdir("Dir1")
 os.chdir("Dir1")
-path = os.getcwd()
 os.mkdir("Dir2")
 os.mkdir("Dir3")
 os.chdir("Dir3")
@@ -21,12 +21,11 @@ while user_choice == "yes" and os.getcwd() != path:
             except:
                 os.chdir("..")
         else:
-            print(os.listdir())
             for item in os.listdir():
-                print(item)
                 if os.path.isdir(item):
                     os.rmdir(item)
                 elif os.path.isfile(item) and item == "test.rtf":
                     os.remove(item)
                     break
                     
+
