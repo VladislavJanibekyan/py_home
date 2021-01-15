c0485c2045a993a35e8b6fe00a324c428ed90c93
Vladislav <v.janibekyan@gmail.com>
2021-01-15 12:12:57 +0200

    more bugs fixed

--- os_home.py
+++ os_home.py
@@ -1,32 +1,29 @@
 import os
 print("begining:", os.getcwd())
+path = os.getcwd()
 os.mkdir("Dir1")
 os.chdir("Dir1")
-path = os.getcwd()
 os.mkdir("Dir2")
 os.mkdir("Dir3")
 os.chdir("Dir3")
 os.mkdir("Dir4")
-os.chdir("Dir4")
 path_end = os.getcwd()
 print(path_end)
-path_end.split("/")
-user_choice = input("which directory or file do you want to delete? ")
+path_list = path_end.split("/")
+user_choice = input("do you want to delete all new created directories and test.rtf file? ")
 
 while user_choice == "yes" and os.getcwd() != path:
-    for i in path_end[::-1]:
+    for i in path_list[:-4:-1]:
         if not os.listdir(): 
             try:
                 os.rmdir()
             except:
                 os.chdir("..")
         else:
-            print(os.listdir())
             for item in os.listdir():
-                print(item)
-                if os.path.isdir(item):
+                if os.path.isdir(item) and item == "Dir1" or item == "Dir2" or item == "Dir3" or item == "Dir4":
                     os.rmdir(item)
                 elif os.path.isfile(item) and item == "test.rtf":
                     os.remove(item)
-                    break
+                
                     
